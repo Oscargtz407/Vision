c2273a288181816d29c1eed904ed952114166c36
Oscargtz407 <slendermus234.0@gmail.com>
2023-11-22 19:17:23 -0600

    second commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Vision3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Vision3.java
@@ -7,9 +7,9 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -29,7 +29,7 @@ public class Vision3 extends OpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("CameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
 
-        camera1.setPipeline(new examplePipeline());
+        camera1.setPipeline(new ExamplePipeline());
 
         camera1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
@@ -45,70 +45,49 @@ public class Vision3 extends OpMode {
 
     @Override
     public void loop() {
-
+        // No es necesario implementar el loop para este caso
     }
 
-    class examplePipeline extends OpenCvPipeline {
-        Mat YCbCr = new Mat();
-        Mat hsvLeft = new Mat();
-        Mat hsvRight = new Mat();
-        Mat maskLeft = new Mat();
-        Mat maskRight = new Mat();
-        Mat hierarchyLeft = new Mat();
-        Mat hierarchyRight = new Mat();
-        List<MatOfPoint> contoursLeft = new ArrayList<>();
-        List<MatOfPoint> contoursRight = new ArrayList<>();
-        Mat outPut = new Mat();
+    class ExamplePipeline extends OpenCvPipeline {
         Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
 
         @Override
         public Mat processFrame(Mat input) {
-            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
-            telemetry.addLine("Center");
+            Mat hsv = new Mat();
+            Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2YCrCb);
 
-            Rect leftRect = new Rect(1, 1, 425, 719);
-            Rect rightRect = new Rect(850, 1, 425, 719);
+            Rect chessTowerRect = new Rect(1, 1, 425, 719);
 
-            input.copyTo(outPut);
-            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
-            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
+            Mat chessTowerCrop = hsv.submat(chessTowerRect);
 
-            Mat leftCrop = YCbCr.submat(leftRect);
-            Mat rightCrop = YCbCr.submat(rightRect);
+            Scalar lowerBound = new Scalar(0, 100, 100); // Bajo límite de rojo
+            Scalar upperBound = new Scalar(10, 255, 255); // Alto límite de rojo
 
-            // Convertir a HSV para detectar mejor el color de la torre (izquierda)
-            Imgproc.cvtColor(leftCrop, hsvLeft, Imgproc.COLOR_YCrCb2RGB);
-            Scalar lowerBoundLeft = new Scalar(20, 50, 50); // Ajusta según el color de la torre
-            Scalar upperBoundLeft = new Scalar(30, 255, 255); // Ajusta según el color de la torre
-            Core.inRange(hsvLeft, lowerBoundLeft, upperBoundLeft, maskLeft);
+            Mat mask = new Mat();
+            Core.inRange(chessTowerCrop, lowerBound, upperBound, mask);
 
-            // Convertir a HSV para detectar mejor el color de la torre (derecha)
-            Imgproc.cvtColor(rightCrop, hsvRight, Imgproc.COLOR_YCrCb2RGB);
-            Scalar lowerBoundRight = new Scalar(20, 50, 50); // Ajusta según el color de la torre
-            Scalar upperBoundRight = new Scalar(30, 255, 255); // Ajusta según el color de la torre
-            Core.inRange(hsvRight, lowerBoundRight, upperBoundRight, maskRight);
+            List<MatOfPoint> contours = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-            // Encontrar contornos en la máscara (izquierda)
-            Imgproc.findContours(maskLeft, contoursLeft, hierarchyLeft, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-            // Encontrar contornos en la máscara (derecha)
-            Imgproc.findContours(maskRight, contoursRight, hierarchyRight, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            Mat output = input.clone();
+            Imgproc.rectangle(output, chessTowerRect, rectColor, 2);
 
-            // Dibujar contornos en la imagen de salida (izquierda)
-            Imgproc.drawContours(outPut, contoursLeft, -1, new Scalar(0, 255, 0), 2);
-            // Dibujar contornos en la imagen de salida (derecha)
-            Imgproc.drawContours(outPut, contoursRight, -1, new Scalar(0, 255, 0), 2);
-
-            // Realizar acciones basadas en la presencia de contornos (izquierda)
-            if (contoursLeft.size() > 0) {
-                telemetry.addLine("Contorno de torre de ajedrez detectado en la parte izquierda");
+            for (MatOfPoint contour : contours) {
+                double area = Imgproc.contourArea(contour);
+                if (area > 500 && area < 5000) {  // Ajusta según tus necesidades
+                    double epsilon = 0.02 * Imgproc.arcLength(new MatOfPoint2f(contour.toArray()), true);
+                    MatOfPoint2f approxCurve = new MatOfPoint2f();
+                    Imgproc.approxPolyDP(new MatOfPoint2f(contour.toArray()), approxCurve, epsilon, true);
+                    if (approxCurve.total() == 4) {  // Verificar si el contorno es cuadrado
+                        Imgproc.drawContours(output, contours, -1, new Scalar(0, 255, 0), 2);
+                        telemetry.addLine("Contorno de torre de ajedrez detectado");
+                        break;  // Detener el bucle una vez que se encuentra un contorno válido
+                    }
+                }
             }
 
-            // Realizar acciones basadas en la presencia de contornos (derecha)
-            if (contoursRight.size() > 0) {
-                telemetry.addLine("Contorno de torre de ajedrez detectado en la parte derecha");
-            }
-
-            return outPut;
+            return output;
         }
     }
 }
